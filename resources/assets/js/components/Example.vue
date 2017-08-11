e3094e39b29f6e10d45b76d8c9007169f29cba32
AllenChangCN91 <30739464+AllenChangCN91@users.noreply.github.com>
2017-08-11 20:34:41 +0800

    Update Example.vue

--- resources/assets/js/components/Example.vue
+++ resources/assets/js/components/Example.vue
@@ -6,7 +6,7 @@
                     <div class="panel-heading">Example Component</div>
 
                     <div class="panel-body">
-                        I'm an example 2!
+                        I'm an example 456!
                     </div>
                 </div>
             </div>
