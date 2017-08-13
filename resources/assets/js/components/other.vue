b4b94154c715f02e371ffd3f499e0eb1ff52d43f
AllenChangCN <zhangkailongabc@163.com>
2017-08-13 23:11:47 +0800

    2017年8月13日 子路由功能实现

---
+++ resources/assets/js/components/other.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        this is the compnent other!
+    </div>
+</template>
+
+<script></script>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,20 @@ let mix = require('laravel-mix');
  |
  */
 
+// reference:https://stackoverflow.com/questions/42047874/laravel-5-4-mix-how-to-run-browser-live-reload
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+mix.webpackConfig({
+    plugins: [
+        new BrowserSyncPlugin({
+            files: [
+                'app/**/*',
+                'public/**/*',
+                'resources/views/**/*',
+                'routes/**/*'
+            ]
+        })
+    ]
+});
+
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
